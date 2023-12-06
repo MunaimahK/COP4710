e947d297a3753d1ab9ddae8929103d8939900676
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2023-12-05 20:09:03 -0500

    Refactor login function and update HTML structure

--- login.php
+++ login.php
@@ -1,69 +1,51 @@
 <!doctype html>
 <html lang="en">
 <?php
+require('utils.php');
+function login()
+{
+  $email = $_POST["email"];
+  $password = $_POST["password"];
 
-    function login()
-    {
+  $conn = createConnection();
+  $sql = "SELECT * FROM Users WHERE Email='$email' AND Password='$password'";
+  $result = $conn->query($sql);
 
-        $host = "localhost";
-        $username = "root";
-        $pwd = "";
-        $db_name = "COP4710";
+  if ($result->num_rows == 1) {
+    header('Location: /ship-registration.html');
+  } else {
+    header('Location: /signup.php');
+  }
 
-        $email = $_POST["email"] ?? null;
-        $password = $_POST["password"] ?? null;
-
-        $conn = new mysqli($host, $username, $pwd, $db_name);
-        $query = "SELECT * FROM Users WHERE Email='$email' AND Password='$password'";
-        $result = $conn->query($query);
-
-        if($result->num_rows == 1)
-        {
-          // Login Succesfull; tuple found
-          // print_r("Success!");
-          header('Location: https://localhost/ship-registration.html');
-          exit();
-
-        }
-        else {
-          // login failed;
-         header('Location: https://localhost/signup.php');
-          exit();
-        }
-
-        $conn->close();
-    }
+  $conn->close();
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  login();
+}
 ?>
 
 
 
 <head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
   <link rel="stylesheet" href="styles.css" />
   <script src="navbar.js"></script>
 </head>
 
 <body>
-<navbar-component></navbar-component>
-    <main>
+  <navbar-component></navbar-component>
+  <main>
     <h1>Login</h1>
-    
     <form action="login.php" method="post">
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email">
+      <label for="email">Email</label>
+      <input type="email" id="email" name="email">
       <div>
         <label for="password">Password</label>
         <input type="password" id="password" name="password">
       </div>
       <button>Log In</button>
-      
     </form>
-    <?php
-         login();
-        ?>
-
-    </main>
-
-    </body>
+  </main>
+</body>
 
 </html>
